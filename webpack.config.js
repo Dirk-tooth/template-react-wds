028b20d8302d99f9f8b5232782a4d92cb6c7ebd8
Dirk-Tooth <trevormckay.davis@gmail.com>
2017-02-14 15:59:33 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: './src/main.js', // this is the path to your main JS file
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader?attrs=false',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  devtool: 'cheap-source-map',
+  debug: true,
+  output: {
+    path: './bin', // path to where you want the built file
+    publicPath: '/bin',
+    filename: 'bundle.js', // name you want of built file
+  },
+};
